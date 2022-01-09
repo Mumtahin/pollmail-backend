6eb9be005823eb10b06385dc6177583c27cab965
Mumtahin <n.mumtahin1@gmail.com>
2022-01-09 22:06:17 +0300

    remove gitignore files

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// route handlers
 app.get('/', (req, res) => {
     res.send({hi: 'there'});
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
