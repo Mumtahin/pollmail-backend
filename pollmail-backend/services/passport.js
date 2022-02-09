b17a7676b65944ae8756d49644b6eb7d4209c425
Mumtahin <n.mumtahin1@gmail.com>
2022-02-09 00:55:20 +0000

    add user schema and init passport callback

--- pollmail-backend/services/passport.js
+++ pollmail-backend/services/passport.js
@@ -1,16 +1,32 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
+
 passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken', accessToken);
-        console.log('refreshToken', refreshToken);
-        console.log('profile', profile);
-    })
+	new GoogleStrategy({
+		clientID: keys.googleClientID,
+		clientSecret: keys.googleClientSecret,
+		callbackURL: '/auth/google/callback'
+	},
+		(accessToken, refreshToken, profile, done) => {
+			User.findOneAndDelete({ googleId: profile.id })
+				.then((existingUser) => {
+					if (existingUser) {
+						console.log('existing user');
+						console.log(existingUser.googleId, existingUser.firstName, existingUser.surname);
+						done(null, existingUser);
+					} else {
+						console.log('not existing user');
+						new User({
+							googleId: profile.id,
+							firstName: profile.name.givenName,
+							surname: profile.name.familyName
+						}).save()
+							.then(user => done(null, user));
+					}
+				})
+		})
 );
